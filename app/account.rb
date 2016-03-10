5271a24935abc4e53e74755beab68e808611df7d
Jason Tham <jason.tham@tempus.com>
2016-03-09 19:29:23 -0600

    finished account object

---
+++ app/account.rb
@@ -0,0 +1,36 @@
+class Account
+  attr_reader :value
+  attr_accessor :transactions
+  def initialize()
+    @value = 0.00
+    @transactions = []
+  end
+
+  def deposit(money)
+    if money >= 0
+      @value += money
+      transactions.push(Transactions.new(money, "deposit"))
+    else
+      "Cannot deposit negative value"
+    end
+
+  end
+
+  def withdraw(money)
+    if money >= 0
+      @value -= money
+      transactions.push(Transactions.new(money, "withdraw"))
+    else
+      "Cannot withdraw negative value"
+    end
+  end
+
+  private
+  class Transactions
+    attr_reader :value, :action
+    def initialize(money, action)
+      @value = money
+      @action = action
+    end
+  end
+end
---
+++ spec/account_spec.rb
@@ -0,0 +1,80 @@
+require_relative '../app/account'
+
+describe Account do
+  let(:acc) { Account.new }
+  describe '#value' do
+    it 'has an initial value of zero dollars' do
+      expect(acc.value).to eq 0.00
+    end
+  end
+
+  describe '#deposit' do
+    it 'adds deposit to value' do
+      acc.deposit(100.00)
+      expect(acc.value).to eq 100.00
+    end
+
+    it 'saves deposit transaction to transaction history' do
+      acc.deposit(100.00)
+      trans = acc.transactions.last
+
+      expect(trans.value).to eq 100.00
+      expect(trans.action).to eq 'deposit'
+    end
+
+    context 'negative deposit' do
+      before do
+        acc.deposit(-100.00)
+      end
+
+      it 'should return value of 0.00' do
+        expect(acc.value).to eq 0.00
+      end
+
+      it 'does not create a transactions' do
+        expect(acc.transactions.count).to eq 0
+      end
+
+      it 'returns a deposit error message' do
+        expect(acc.deposit(-100)).to eq "Cannot deposit negative value"
+      end
+    end
+  end
+
+  describe '#withdraw' do
+    before do
+      acc.deposit(100.00)
+    end
+
+    it 'withdraws from value' do
+      acc.withdraw(25.00)
+      expect(acc.value).to eq 75.00
+    end
+
+    it 'creates withdraw transaction' do
+      acc.withdraw(25.00)
+      trans = acc.transactions.last
+
+      expect(trans.value).to eq 25.00
+      expect(trans.action).to eq 'withdraw'
+    end
+
+    context 'with negative value' do
+      before do
+        acc.withdraw(-25.00)
+      end
+
+      it 'should return original value' do
+        expect(acc.value).to eq 100.00
+      end
+
+      it 'does not add transaction to transactions' do
+        expect(acc.transactions.count).to eq 1
+      end
+
+      it 'returns withdraw error message' do
+        expect(acc.withdraw(-25.00)).to eq "Cannot withdraw negative value"
+      end
+    end
+  end
+end
